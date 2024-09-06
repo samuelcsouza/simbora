7a8a8625dd624cf4b8234251b3790f71372d4dd3
samuelcsouza <samuel_dacruz@hotmail.com>
2024-09-06 13:59:06 -0300

    feat: publisher and subscriber

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # nestjs-kafka
+
+References
+[Microservices implementation using Kafka and NestJS](https://medium.com/@mohsenmahoski/microservices-implementation-using-kafka-and-nestjs-aee085ca17a4)
---
+++ api-gateway/src/app.controller.ts
@@ -0,0 +1,68 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { Admin } from '@nestjs/microservices/external/kafka.interface';
+import { Kafka } from 'kafkajs';
+import { fibonacci } from './util';
+
+@Controller()
+export class AppController {
+  private admin: Admin;
+  constructor(@Inject('FIBO_SERVICE') private client: ClientKafka) {}
+
+  @Get('/fibonacci')
+  async getFibo() {
+    return fibonacci(2);
+  }
+
+  private getFiboResult() {
+    return new Promise((resolve) => {
+      this.client
+        .send('fibo', JSON.stringify({ num: 16 }))
+        .subscribe((result: number) => {
+          console.log('>>>>>>>>>>>>>>>>>>>>>>>>', result);
+          resolve(result);
+        });
+    });
+  }
+
+  @Get('/microservice-fibonacci')
+  async getFibonacci() {
+    const fibo = await this.getFiboResult();
+    console.log('send message!');
+    console.log(fibo);
+    return fibo;
+  }
+
+  async onModuleInit() {
+    this.client.subscribeToResponseOf('fibo');
+    const kafka = new Kafka({
+      clientId: 'my-app',
+      brokers: ['localhost:29092'],
+    });
+    this.admin = kafka.admin();
+    const topics = await this.admin.listTopics();
+
+    const topicList = [];
+    if (!topics.includes('fibo')) {
+      topicList.push({
+        topic: 'fibo',
+        numPartitions: 10,
+        replicationFactor: 1,
+      });
+    }
+
+    if (!topics.includes('fibo.reply')) {
+      topicList.push({
+        topic: 'fibo.reply',
+        numPartitions: 10,
+        replicationFactor: 1,
+      });
+    }
+
+    if (topicList.length) {
+      await this.admin.createTopics({
+        topics: topicList,
+      });
+    }
+  }
+}
