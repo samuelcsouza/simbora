ec44e04b8ee59c184dbc24b809211229b1dd9421
samuelcsouza <samuel_dacruz@hotmail.com>
2024-09-09 14:49:26 -0300

    style: rename controller methods

--- consumer/src/app.controller.ts
+++ consumer/src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
   constructor(private observationService: ObservationService) {}
 
   @MessagePattern('636b4c0f-4490-4213-ba53-db21b44c97b0')
-  async getFibonacci(@Payload() message: { payload: string }) {
+  async getDevice1Message(@Payload() message: { payload: string }) {
     console.log(
       `new message from topic 636b4c0f-4490-4213-ba53-db21b44c97b0 | `,
       message,
@@ -25,7 +25,7 @@ export class AppController {
   }
 
   @MessagePattern('f65de111-18d2-4cfc-b367-80d208748490')
-  async getDevicePayload(@Payload() message: { payload: string }) {
+  async getDevice2Message(@Payload() message: { payload: string }) {
     console.log(
       `new message from topic f65de111-18d2-4cfc-b367-80d208748490 | `,
       message,
@@ -42,7 +42,7 @@ export class AppController {
   }
 
   @MessagePattern('e22c2e51-ed9f-4e7e-9c2b-e2afa0ad3003')
-  async getDevicePayload2(@Payload() message: { payload: string }) {
+  async getDevice3Message(@Payload() message: { payload: string }) {
     console.log(
       `new message from topic e22c2e51-ed9f-4e7e-9c2b-e2afa0ad3003 | `,
       message,
