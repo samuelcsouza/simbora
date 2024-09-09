31032acce2c75e53ac3a6609d865462bd774610d
samuelcsouza <samuel_dacruz@hotmail.com>
2024-09-09 16:37:58 -0300

    fix: revert payload parser method location

--- consumer/src/observation/observation.service.ts
+++ consumer/src/observation/observation.service.ts
@@ -1,33 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { ObservationRepository } from './observation.repository';
 import { Observation } from './observation.entity';
-import { DevicePayload, DevicePayloadParsed } from 'src/device/device.entity';
+import { DevicePayloadParsed } from 'src/device/device.entity';
 
 @Injectable()
 export class ObservationService {
   constructor(private observationRepository: ObservationRepository) {}
 
-  async insert(deviceId: string, payload: DevicePayload): Promise<Observation> {
-    const payloadParsed = await this.payloadParser(payload);
-
+  async insert(
+    deviceId: string,
+    payloadParsed: DevicePayloadParsed,
+  ): Promise<Observation> {
     return await this.observationRepository.sendObservationData(
       deviceId,
       payloadParsed,
     );
-  }
-
-  private async payloadParser(
-    payload: DevicePayload,
-  ): Promise<DevicePayloadParsed> {
-    const dataSplit = payload.payload.split(';');
-
-    const parsed: DevicePayloadParsed = {
-      deviceTimestamp: dataSplit[0].toString(),
-      deviceValue: dataSplit[1].toString(),
-      deviceVariable: dataSplit[2].toString(),
-      deviceUnit: dataSplit[3].toString(),
-    };
-
-    return parsed;
   }
 }
